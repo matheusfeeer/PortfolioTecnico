26a2d770d24bf76afe669df6c776a447cac1112c
matheusfeeer <82882142+matheusfeeer@users.noreply.github.com>
2021-06-16 09:24:28 -0300

    Atualizar varia´vel

--- FundamentosTI/exemplos/16-06/while.sh
+++ FundamentosTI/exemplos/16-06/while.sh
@@ -11,6 +11,6 @@ INPUT_STRING=”hello”                              #Variável criada pra ser 
 while [[ $INPUT_STRING != "bye" ]]                #Se a String for diferente de bye, faça:
 do                                                #Abrir o while
   echo "Please type something in (bye to quit)"   #Mensagem para orientar o usuário (Se escrever bye ele sai, conforme escrito na linha 11)
-  read INPUT_STRING                               #Ler a variável criada anteriormente
+  read INPUT_STRING                               #Atualizar a variável criada anteriormente
   echo "You typed: $INPUT_STRING"                 #Dizer o que o usuário escreveu
 done                                              #Fechar o while
