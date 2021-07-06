2244975f34b112928bdf3e63c12e6188b6643518
matheusfeeer <82882142+matheusfeeer@users.noreply.github.com>
2021-07-06 09:38:05 -0300

    Update exercicio2.sh

--- FundamentosTI/Dia06DeJulho/exercicio2.sh
+++ FundamentosTI/Dia06DeJulho/exercicio2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 #Iniciar código
+#ESSE É EM LOOP
 
 teste=0 #variável teste é 0
 for i in $( ls ) #Para i = variável ls do diretório
@@ -16,3 +17,18 @@ then #Faça
         echo "Criando arquivo $1" #Avisa que vai criar o arquivo da variável 1
         touch $1 #Cria o arquivo
 fi #Finaliza
+
+  
+#!/bin/bash
+#Inicia o código
+#ESSE É EM VETOR
+
+var=$(ls) #Variável = listagem do diretório vetor
+var1=${var/$1} # $1 é o primeiro argumento enviado para o script
+if [[ $var != $var1 ]] #Se a primeira variável foi diferente de $var1
+then #Faça
+	echo "O arquivo existe" #Escrever que o arquivo não existe
+else #Caso contrário,
+	echo "Criando o arquivo $1 ..." #Avisa que está criando o arquivo (nome da variável $1)
+	touch $1 #Cria o arquivo
+fi #Finaliza o código
