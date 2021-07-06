52cbfb44817cf759840bc4aae9ca34df0f0fa894
matheusfeeer <82882142+matheusfeeer@users.noreply.github.com>
2021-07-06 09:42:23 -0300

    Update exercicio1.sh

--- FundamentosTI/Dia06DeJulho/exercicio1.sh
+++ FundamentosTI/Dia06DeJulho/exercicio1.sh
@@ -32,6 +32,17 @@ then #Faça
         touch $1 #Cria o arquivo
 fi #Finaliza
 
+#!/bin/bash
+#Inicia o código
+
+var=$(cat /etc/passwd | grep $1) #Variável = procura se a variável $1 existe dentro de /etc/passwd
+if [ -z $var ] # -z  se a variavel estiver vazia
+then #Faça
+        echo "O usuário n existe no sistema" #Escrever que o usuário não existe no sistema
+else #Caso contrário,
+        echo "O usuário já está cadastrado" #Escrever que o usuário já esta cadastrado
+fi #Finalizar o if
+
   
 #!/bin/bash
 #Inicia o código
