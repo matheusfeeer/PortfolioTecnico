69f020e917c493fa3b403d5a21fa740f7413fbc8
matheusfeeer <82882142+matheusfeeer@users.noreply.github.com>
2021-07-06 19:50:40 -0300

    Update exercicio3.sh

--- FundamentosTI/Dia06DeJulho/exercicio3.sh
+++ FundamentosTI/Dia06DeJulho/exercicio3.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #Inicia o código
 
-var=$(cat /etc/passwd | grep $1) #Variável = procura se a variável $1 existe dentro de /etc/passwd
-if [ -z $var ] # -z  se a variavel estiver vazia
-then #Faça
-        echo "O usuário n existe no sistema" #Escrever que o usuário não existe no sistema
-else #Caso contrário,
-        echo "O usuário já está cadastrado" #Escrever que o usuário já esta cadastrado
-fi #Finalizar o if
+if [ $1 -gt $2 ]
+then
+echo "$1 é maior que $2."
+
+elif [ $2 -gt $1 ]
+then
+echo "$2 é maior que $1."
+
+else [ $1 -eq $2 ]
+then
+echo "$1 e $2 são iguais."
+
+fi
